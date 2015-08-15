0d5e3581c9a465571cd3276de601517c6bf7019d
Tran Xuan Viet <viettranx@gmail.com>
2015-08-15 10:56:01 +0700

    Remove interval on youtube script

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -1,18 +1,13 @@
-//$(function() {
-	$(document).ready(function() {
-		setInterval(function() {
-			$('div.youtube').each(function() {
-				yID = $(this).attr('id');
-				yW	= $(this).attr('yW') ? $(this).attr('yW') : 420;
-				yH	= $(this).attr('yH') ? $(this).attr('yH') : 315;
+$(function() {
+	
+	$('div.youtube').each(function() {
+		yID = $(this).attr('id');
+		yW	= $(this).attr('yW') ? $(this).attr('yW') : 420;
+		yH	= $(this).attr('yH') ? $(this).attr('yH') : 315;
 
-				$(this).append('<iframe width="'+yW+'" height="'+yH+'" src="https://www.youtube.com/embed/'+yID+'" frameborder="0" allowfullscreen></iframe>').attr('class', '');
+		$(this).append('<iframe width="'+yW+'" height="'+yH+'" src="https://www.youtube.com/embed/'+yID+'" frameborder="0" allowfullscreen></iframe>').attr('class', '');
 
-			});
-		}, 1000);
-
-		
 	});
 	
-//});
+});
 
